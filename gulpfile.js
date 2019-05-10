876ac3472b3c17b1cc45e12d3f67480f5dc686aa
Siren Studio 4 <sirenstudio4@studio4.local>
2019-05-10 16:24:04 +0100

    put in map fallback images

--- gulpfile.js
+++ gulpfile.js
@@ -8,8 +8,8 @@ const gulp = require("gulp"),
   autoprefixer = require("gulp-autoprefixer"),
   sass = require("gulp-sass"),
   rename = require("gulp-rename"),
-  gutil = require('gulp-util'),
-  terser = require('gulp-terser');
+  gutil = require("gulp-util"),
+  terser = require("gulp-terser");
 
 /*
 --TOP LEVEL FUCNTIONS--
@@ -20,17 +20,18 @@ gulp.watch - Watch files & folders for changes
 */
 
 //Move folders
-gulp.task("folders", function () {
-  gulp.src([
+gulp.task("folders", function() {
+  gulp
+    .src([
       //'src/libs/**',
       //'src/fonts/**',
-      'src/assets/webfonts/**'
+      "src/assets/webfonts/**"
     ])
     .pipe(gulp.dest("dist/assets/webfonts"));
 });
 
 //Move HTML files
-gulp.task("html", function () {
+gulp.task("html", function() {
   gulp.src("src/*.html").pipe(gulp.dest("dist"));
 });
 
@@ -54,16 +55,16 @@ gulp.task("html", function () {
 
 //OPTIMISE IMAGES 2
 //npm install -D imagemin-pngquant imagemin-jpeg-recompress
-gulp.task("imagemin", function () {
+gulp.task("imagemin", function() {
   return gulp
     .src("src/assets/img/*")
     .pipe(
       imagemin([
         //png files lossy
-        // imageminPngquant({
-        //   speed: 3, //1 bruteforce => 11 fastest
-        //   quality: [0.2, 0.4] //lossy settings 0-1 if too low wont save
-        // }),
+        imageminPngquant({
+          speed: 8, //1 bruteforce => 11 fastest
+          quality: [0.9, 1] //lossy settings 0-1 if too low wont save
+        })
 
         // //jpg lossless
         // imagemin.jpegtran({
@@ -97,7 +98,7 @@ gulp.task("imagemin", function () {
 
 //Concatenate JS files & Minify JS
 //npm install --save-dev gulp-uglify
-gulp.task("uglify", function () {
+gulp.task("uglify", function() {
   gulp
     .src("src/assets/js/*.js")
     //.pipe(concat("main.js"))
@@ -113,53 +114,33 @@ gulp.task("uglify", function () {
     .pipe(gulp.dest("dist/assets/js"));
 });
 
-//Compile SASS
-//npm install --save-dev gulp-sass
-// gulp.task('sass', function(){
-//   gulp.src('src/scss/*.scss')
-//     .pipe(sass().on('error',sass.logError))
-//     .pipe(gulp.dest('dist/src/css'))
-//     .pipe(gulp.dest('css/'))
-// })
-
 // Compile Sass to css and place it in dist and local folders along with minified version
 //npm install --save-dev gulp-sass gulp-concat gulp-autoprefixer
-gulp.task("sass", function () {
+gulp.task("sass", function() {
   return (
     gulp
-    .src("src/assets/scss/*.scss")
-    .pipe(
-      sass({
-        "sourcemap=none": true
-      })
-    )
-    .pipe(concat("main.css"))
-    .pipe(autoprefixer("last 2 version", "ie 9"))
-    .pipe(gulp.dest("src/assets/css/"))
+      .src("src/assets/scss/*.scss")
+      .pipe(
+        sass({
+          "sourcemap=none": true
+        })
+      )
+      .pipe(concat("main.css"))
+      .pipe(autoprefixer("last 2 version", "ie 9"))
+      .pipe(gulp.dest("src/assets/css/"))
 
-    // .pipe(rename({
-    //     suffix: ".min"
-    // }))
+      // .pipe(rename({
+      //     suffix: ".min"
+      // }))
 
-    .pipe(minifyCss({
-      compatibility: "ie8"
-    }))
-    .pipe(gulp.dest("dist/assets/css"))
+      .pipe(
+        minifyCss({
+          compatibility: "ie8"
+        })
+      )
+      .pipe(gulp.dest("dist/assets/css"))
   );
 });
-
-// Minify CSS
-// gulp.task("minify-css", function () {
-//     return gulp
-//         .src("src/css/styles.css")
-//         .pipe(rename({
-//             suffix: ".min"
-//         }))
-//         .pipe(minifyCss({
-//             compatibility: "ie8"
-//         }))
-//         .pipe(gulp.dest("css"));
-// });
 
 // Catch JS errors
 // npm install jshint gulp-jshint jshint-stylish --save-dev
@@ -170,10 +151,11 @@ gulp.task("sass", function () {
 //     .pipe(jshint.reporter("jshint-stylish"));
 // });
 
-// Watch for changes in scss files
+// Watch for changes in scss & html & other files
 // Watch for errors in js file
-gulp.task("watch", function () {
+gulp.task("watch", function() {
   gulp.watch("src/assets/scss/**/*.scss", ["sass"]);
+  gulp.watch("src/*.html", ["html"]);
   // gulp.watch("src/assetsjs/*.js", ["uglify"]);
 });
 
--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
-Run 'gulp' or 'gulp watch' in the terminal and use live server.
+1. Run 'gulp watch' in the terminal and use VSC live server plugin.
+
+2. Run 'gulp' for final build of assets to 'dist' folder.
